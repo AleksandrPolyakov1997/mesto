a89faa53a6c9016f4d9bdf8cae20090a4acec821
Aleksandr Polyakov <aleksandrpoliakoff@yandex.ru>
2022-12-04 23:48:14 +0300

    fix: edits on the first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,18 @@ let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name-input');
-let jobInput = formElement.querySelector('.popup__job-input');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_job');
 
 function openPopup() {
-  popup.classList.add('popup__open');
+  popup.classList.add('popup_open');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup__open');
+  popup.classList.remove('popup_open');
 }
-
-closeButton.addEventListener('click', closePopup);
-editButton.addEventListener('click', openPopup);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -29,4 +26,6 @@ function handleFormSubmit (evt) {
     closePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
+formElement.addEventListener('submit', handleFormSubmit);
